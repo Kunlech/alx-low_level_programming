8543d268e6eb4d4385959f21e47d88fe6214dafd
Kunlech <kunlech001@gmail.com>
2022-03-13 00:27:51 -0800

    updated

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,53 +1,37 @@
-include <stdio.h>
-
+#include <stdio.h>
 /**
-
  * main - Program entry point
-
  *
-
  * Return: 0 on success. Error code otherwise
-
  */
-
 int main(void)
-
 {
-
+	
 char a = 'a';
-
 char A = 'A';
 
 int n = 0;
 
-
-
 while (n < 52)
 
 {
-
 if (n < 26)
+}
 
 {
-
 putchar(a);
-
 a++;
-
 }
 
- else
+else
 
 {
-
 putchar(A);
-
 A++;
-
 }
 
+{
 n++;
-
 }
 
 putchar('\n');
