1cc611dcc533e8db3d31de59d274c296a2246769
Kunlech <kunlech001@gmail.com>
2022-03-13 08:53:18 -0700

    updated

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+
+int main(void)
+{
+
+for (int i = 0; i <= 9; i++)
+{
+putchar(i + '0');
+putchar(j + '0');
+if (((i * 10) + j) < 89)
+{
+purchar(',');
+purchar(' ');
+}
+}
+putchar('\n');
+return (0);
+
+}
