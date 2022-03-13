49dc03e8b85ad9f1edfcd1dac5c806e80c2ecb15
Kunlech <kunlech001@gmail.com>
2022-03-13 09:02:07 -0700

    updated

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,12 +10,15 @@ int main(void)
 
 for (int i = 0; i <= 9; i++)
 {
+for (int j = i + 1; j <= 9; j++)
+{
 putchar(i + '0');
 putchar(j + '0');
 if (((i * 10) + j) < 89)
 {
 purchar(',');
 purchar(' ');
+}
 }
 }
 putchar('\n');
