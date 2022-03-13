3b5c4cfeee125434786056dfe136b353766a2036
Kunlech <kunlech001@gmail.com>
2022-03-13 08:20:46 -0700

    updated

---
+++ 100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (sucess/correct)
+ */
+int main(void)
+{
+for (int i = 0; i <= 9; i++)
+{
+putchar(i + '0');
+putchar(j + '0');
+if (((i * 10) + j) < 89)
+{
+putchar (',');
+puchar(' ');
+}
+
+}
+
+
+putchar('\n');
+return (0);
+
+}
